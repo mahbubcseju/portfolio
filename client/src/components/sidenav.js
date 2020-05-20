a7df4c58433ae3ad619011f4e22857dae530213c
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-05-20 21:09:06 +0900

    Added round image and button for chaning

--- client/src/components/sidenav.js
+++ client/src/components/sidenav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import '../static/css/sidenav.css'
+import '../static/css/sidenav.css';
+import proimage from '../images/P-2481.jpg';
 
 class SideNav extends React.Component {
   componentDidMount() {
@@ -10,6 +11,12 @@ class SideNav extends React.Component {
     return (
       <div>
         <div className="side-nav">
+          <div className="nav-image">
+            <img src={proimage} className="ui circular image pro-image"/>
+            <div className="image-button">
+              <i className="edit icon"/>
+            </div>
+          </div>
           <div className="nav-item">
             <i className="user icon"></i><NavLink exact to="/" activeClassName="active"> Profile</NavLink>
           </div>
