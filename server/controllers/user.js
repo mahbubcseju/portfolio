2560d0da13e88ad12bc921dfb916f5be7e1d5c8f
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-05-24 20:57:13 +0900

    Added image uploader

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,25 @@
 ProfilePicture = require('../models/user.js');
+Uploader = require("../services/profileUploader.js");
 
 exports.store_profile_image = function(req, res) {
-  var newProfileImage = new ProfilePicture(req.body);
-  newProfileImage.save()
-  .then(data => {
-    res.send(data);
-  }).catch(err => {
-    res.status(500).send({
-      message: "Error occured when creating"
+  const uploader = Uploader.upload.single("image");
+  uploader(req, res, function(err){
+    if(err) {
+      return res.status(500).send({
+        message: "Error in uploading" 
+      });
+    }
+    var newProfileImage = new ProfilePicture({
+      email: req.body.email,
+      image: req.file.path
+    });
+    newProfileImage.save()
+    .then(data => {
+      res.send(data);
+    }).catch(err => {
+      res.status(500).send({
+        message: "Error occured when creating"
+      });
     });
   });
 };
